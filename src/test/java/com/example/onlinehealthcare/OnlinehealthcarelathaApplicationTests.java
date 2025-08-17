3bd5169043d748820fa665cb87e318db05ee8308
Latha-yata <lathayata1234@gmail.com>
2025-08-17 20:19:17 +0530

    Initial commit for fresh Java project

---
+++ src/test/java/com/example/onlinehealthcare/OnlinehealthcarelathaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinehealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinehealthcarelathaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
